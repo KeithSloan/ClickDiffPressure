208559afab1a1314d2fe43204ab0830c25104ca5
Keith Sloan <keith@sloan-home.co.uk>
2016-11-01 20:33:11 +0000

    Now working to select Letter

--- LetterBoard.py
+++ LetterBoard.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def initTable() :
+   global Table
    Table = [[0 for y in range(5)] for x in range(6)]
    Table [0] [0] = "A"
    Table [0] [1] = "E"
@@ -29,6 +30,39 @@ def initTable() :
    Table [5] [3] = "T"
    Table [5] [4] = "Z"
 
+def initLED():
+# Slot 2 pins
+  global LRpin,LYpin,LGpin,RRpin,RYpin,RGpin
+  LRpin = 13
+  LYpin = 19
+  LGpin = 14
+  RRpin = 17
+  RYpin = 26
+  RGpin = 15
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(LRpin, GPIO.OUT)
+  GPIO.setup(LYpin, GPIO.OUT)
+  GPIO.setup(LGpin, GPIO.OUT)
+  GPIO.setup(RRpin, GPIO.OUT)
+  GPIO.setup(RYpin, GPIO.OUT)
+  GPIO.setup(RGpin, GPIO.OUT)
+
+def indicateVertScan():
+    GPIO.output(LGpin,GPIO.HIGH)
+    GPIO.output(RGpin,GPIO.HIGH)
+    GPIO.output(LRpin,GPIO.LOW)
+    GPIO.output(RRpin,GPIO.LOW)
+    global mode
+    mode = 0
+
+def indicateHorzScan():
+    GPIO.output(LRpin,GPIO.HIGH)
+    GPIO.output(RRpin,GPIO.HIGH)
+    GPIO.output(LGpin,GPIO.LOW)
+    GPIO.output(RGpin,GPIO.LOW)
+    global mode
+    mode = 1
+
 def timemillisecs():
     dt = datetime.now()
     return (dt.second*1000+dt.microsecond/1000 )
@@ -37,6 +71,8 @@ import spidev
 import time
 import sys
 from datetime import datetime
+import RPi.GPIO as GPIO
+initLED()
 spi = spidev.SpiDev()			# create spi object
 spi.open(0, 0)				# open spi port 0, device (CS) 1
 spi.mode = 0b11				# 1,1 - 3 bytes read; 0,0 - 4 bytes read# State 0 - Low Value
@@ -48,6 +84,7 @@ timeout = 1500
 state = 0
 count = 0
 initTable()
+indicateVertScan()
 try:
     while True:
         resp = spi.readbytes(3)		# read three bytes
@@ -72,11 +109,19 @@ try:
 		 t3 = timemillisecs()
 		 if ((t3 - t2) > timeout ) :
 		    print "TIME OUT : Count "+str(count)
+                    if ( mode == 0 ) :	# End VertScan
+			y = count - 1
+			indicateHorzScan()
+		    else : 		# End HorzScan
+			x = count - 1
+			print"Letter Selected : "+Table[x][y]
+			indicateVertScan()
 		    state = 0
 		    count = 0
 	time.sleep(0.1)			# sleep for 0.1 secs
     # end while
 except KeyboardInterrupt :
+    GPIO.cleanup()
     spi.close()
     print
 

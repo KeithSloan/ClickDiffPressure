2fc6745f182986df0bd8e2427066afc954f3b9a5
Keith Sloan <keith@sloan-home.co.uk>
2016-11-01 15:20:58 +0000

    Start Musical Instrument

--- LetterBoard.py
+++ LetterBoard.py
@@ -1,24 +1,52 @@
+from datetime import datetime
+
+def timemillisecs():
+    dt = datetime.now()
+    return (dt.second*1000+dt.microsecond/1000 )
+
 import spidev
 import time
 import sys
+from datetime import datetime
 spi = spidev.SpiDev()			# create spi object
 spi.open(0, 0)				# open spi port 0, device (CS) 1
-spi.mode = 0b11				# 1,1 - 3 bytes read; 0,0 - 4 bytes read
-maxVal = 0
-minVal = sys.maxint
+spi.mode = 0b11				# 1,1 - 3 bytes read; 0,0 - 4 bytes read# State 0 - Low Value
+# State 1 - High Value
+# State 2 - Low Value looking for timeout
+debug = len(sys.argv) 
+threshold = 350
+timeout = 1500
+state = 0
+count = 0
 try:
     while True:
         resp = spi.readbytes(3)		# read three bytes
         if (resp[0] != 255) :		# Is conversion valid
            value = 256*resp[0] + resp[1]   # Discard least sig i.e. noise
-	   print resp
-           print value
-           minVal = min(minVal,value)
-           maxVal = max(maxVal,value)
+           if (debug == 2) :
+	      print resp
+              print value
+           if ( value > threshold ) : 	# Signal is HIGH
+              if ( state == 0 ) or (state == 2 ) :
+                 t1 = timemillisecs() 
+                 print "t1 : "+str(t1)
+                 state = 1
+           else :			# Signal is LOW
+              if ( state == 1 ) :
+                 t2 = timemillisecs()
+                 print "t2 : "+str(t2)
+		 count += 1
+                 state = 2
+                 print "Pulse : "+str(t2-t1)
+	      if ( state == 2 ) :
+		 t3 = timemillisecs()
+		 if ((t3 - t2) > timeout ) :
+		    print "TIME OUT : Count "+str(count)
+		    state = 0
+		    count = 0
 	time.sleep(0.1)			# sleep for 0.1 secs
     # end while
 except KeyboardInterrupt :
     spi.close()
     print
-    print "Min : "+str(minVal)+" Max : "+str(maxVal)
 
--- MusicInstrument.py
+++ MusicInstrument.py
@@ -1,11 +1,11 @@
+import psonic
 import spidev
 import time
 import sys
 spi = spidev.SpiDev()			# create spi object
 spi.open(0, 0)				# open spi port 0, device (CS) 1
 spi.mode = 0b11				# 1,1 - 3 bytes read; 0,0 - 4 bytes read
-maxVal = 0
-minVal = sys.maxint
+threshold = 350
 try:
     while True:
         resp = spi.readbytes(3)		# read three bytes
@@ -13,12 +13,11 @@ try:
            value = 256*resp[0] + resp[1]   # Discard least sig i.e. noise
 	   print resp
            print value
-           minVal = min(minVal,value)
-           maxVal = max(maxVal,value)
+           if ( value > threshold ) :
+              play(72)
 	time.sleep(0.1)			# sleep for 0.1 secs
     # end while
 except KeyboardInterrupt :
     spi.close()
     print
-    print "Min : "+str(minVal)+" Max : "+str(maxVal)
 
